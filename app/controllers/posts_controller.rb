4338fd23b81ef60c4851282f97f719d5abc60da1
Elaine-G17 <yilingu2023@u.northwestern.edu>
2023-02-26 01:38:17 +0000

    2nd

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,14 @@ class PostsController < ApplicationController
 
     def index
       @posts = Post.all
-      @user = User.all
     end
   
     def new
       @post = Post.new
-      @user = User.all
     end
   
     def create
       @post = Post.new
-      @post ["user"] = User.all["first_name"]
       @post["body"] = params["post"]["body"]
       @post["image"] = params["post"]["image"]
       @post.save
